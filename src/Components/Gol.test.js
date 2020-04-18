851d58326bca4c9f77dd8511151cd5db410fac0d
AAron Staton <Orson108@hotmail.com>
2020-04-18 16:59:18 -0400

    add -tests for Gol functions

---
+++ src/Components/Gol.test.js
@@ -0,0 +1,70 @@
+// import { updateMatrix } from "./Gol";
+const { updateMatrix, checkNeighbors } = require("../utils");
+
+test("finds live neighboring cells", () => {
+  let testMatrix1 = [
+    [1, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  expect(checkNeighbors(testMatrix1, [1, 1])).toBe(1);
+  let testMatrix2 = [
+    [0, 1, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  expect(checkNeighbors(testMatrix2, [1, 1])).toBe(1);
+  let testMatrix3 = [
+    [0, 0, 1],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  expect(checkNeighbors(testMatrix3, [1, 1])).toBe(1);
+  let testMatrix4 = [
+    [0, 0, 0],
+    [1, 0, 0],
+    [0, 0, 0],
+  ];
+  expect(checkNeighbors(testMatrix4, [1, 1])).toBe(1);
+  let testMatrix5 = [
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 0, 0],
+  ];
+  expect(checkNeighbors(testMatrix5, [1, 1])).toBe(1);
+  let testMatrix6 = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [1, 0, 0],
+  ];
+  expect(checkNeighbors(testMatrix6, [1, 1])).toBe(1);
+  let testMatrix7 = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 1, 0],
+  ];
+  expect(checkNeighbors(testMatrix7, [1, 1])).toBe(1);
+  let testMatrix8 = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 1],
+  ];
+  expect(checkNeighbors(testMatrix8, [1, 1])).toBe(1);
+});
+
+test("Makes center cell live when 3 neighbors are alive", () => {
+  let testMatrix1 = [
+    [1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  let updatedMatrix = updateMatrix(testMatrix1);
+  expect(updatedMatrix[1][1]).toBe(1);
+  let testMatrix2 = [
+    [0, 0, 0],
+    [1, 0, 1],
+    [0, 1, 0],
+  ];
+  updatedMatrix = updateMatrix(testMatrix2);
+  expect(updatedMatrix[1][1]).toBe(1);
+});
